f16f7cc42d1efcf59236556e06d091550839b8bc
Nova Mondal <rialmondal@gmail.com>
2023-04-23 18:31:06 -0700

    initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public final class Constants {
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
+    public static final double DRIVE_POWER = 0.7;
+    public static final double DRIVE_EXPONENT = 1.8;
+    public static final int[] LEFT_DRIVE_MOTOR_PORTS = {3, 4, 5};
+    public static final int[] RIGHT_DRIVE_MOTOR_PORTS = {6, 9, 10};
+    public static final int CURRENT_LIMIT_AMPS = 45;
+}
---
+++ src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,51 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public final class DriveTrain {
+    private DifferentialDrive drive;
+    public DriveTrain() {
+        CANSparkMax Left1 = new CANSparkMax(Constants.LEFT_DRIVE_MOTOR_PORTS[0], MotorType.kBrushless);
+        CANSparkMax Left2 = new CANSparkMax(Constants.LEFT_DRIVE_MOTOR_PORTS[1], MotorType.kBrushless);
+        CANSparkMax Left3 = new CANSparkMax(Constants.LEFT_DRIVE_MOTOR_PORTS[2], MotorType.kBrushless);
+        CANSparkMax Right1 = new CANSparkMax(Constants.RIGHT_DRIVE_MOTOR_PORTS[0], MotorType.kBrushless);
+        CANSparkMax Right2 = new CANSparkMax(Constants.RIGHT_DRIVE_MOTOR_PORTS[1], MotorType.kBrushless);
+        CANSparkMax Right3 = new CANSparkMax(Constants.RIGHT_DRIVE_MOTOR_PORTS[2], MotorType.kBrushless);
+       
+        Left1.setIdleMode(IdleMode.kBrake);
+        Left2.setIdleMode(IdleMode.kBrake);
+        Left3.setIdleMode(IdleMode.kBrake);
+        Right1.setIdleMode(IdleMode.kBrake);
+        Right2.setIdleMode(IdleMode.kBrake);
+        Right3.setIdleMode(IdleMode.kBrake);
+
+        Left1.setSmartCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Left1.setSecondaryCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Left2.setSmartCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Left2.setSecondaryCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Left3.setSmartCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Left3.setSecondaryCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Right1.setSmartCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Right1.setSecondaryCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Right2.setSmartCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Right2.setSecondaryCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Right3.setSmartCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+        Right3.setSecondaryCurrentLimit(Constants.CURRENT_LIMIT_AMPS);
+
+
+        MotorControllerGroup leftMotors = new MotorControllerGroup(Left1, Left2, Left3);
+        MotorControllerGroup rightMotors = new MotorControllerGroup(Right1, Right2, Right3);
+        drive = new DifferentialDrive(leftMotors, rightMotors);
+    }
+
+    public void run(){
+        final double rSpeed = Math.signum(Robot.driverController.getRightY())*Constants.DRIVE_POWER*Math.pow(Math.abs(Robot.driverController.getRightY()), Constants.DRIVE_EXPONENT);
+        // (sign of joystick input) * (drive power) * (absolute value of joystick input to the power of drive exponent)
+        final double lSpeed = Math.signum(Robot.driverController.getLeftY())*Constants.DRIVE_POWER*Math.pow(Math.abs(Robot.driverController.getLeftY()), Constants.DRIVE_EXPONENT);
+        drive.tankDrive(lSpeed, rSpeed);
+    }
+}
